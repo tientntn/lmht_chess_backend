a5577faec15002137d0326182464a3666cd1890b
Giang <ngocgiangit124@gmail.com>
2019-07-15 13:10:00 +0700

    update-

---
+++ app/Http/Controllers/ComboController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Combo;
+use Illuminate\Http\Request;
+use App\Libraries\ImageLib;
+
+use Input, Validator;
+
+class ComboController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $this->data['combos'] = Combo::orderBy('status', 'desc')->get();
+        return view('combos.index', $this->data);
+    }
+
+    public function create() {
+        $this->data['combo'] = new Combo;
+        return view('combos.form', $this->data);
+    }
+
+    public function store () {
+        $rule = [
+            'title'     => 'required',
+            'link'     => 'required',
+        ];
+        $validator = Validator::make(Input::all(), $rule);
+        if ($validator->fails()){
+            return back()->withErrors($validator->messages())->withInput();
+        }
+        $combo = new Combo();
+        $combo->title=Input::get('title');
+        // $slug = str_slug(Input::get('title'));
+        $slug = Input::get('slug');
+        $combo->slug = $combo->checkSlug($slug);
+        $combo->link = Input::get('link');
+        $combo->status = intval(Input::get('status'));
+
+        $combo->save();
+        $combo->cleanCache();
+        return redirect('/combos')->withSuccess('Tạo mới thành công');
+    }
+
+    public function edit($id) {
+        $combo = Combo::find($id);
+        $this->data['combo'] = $combo;
+        if (!$combo) {
+            return view('errors.404');
+        } else {
+            return view('combos.form', $this->data);
+        }
+    }
+
+    public function update ($id) {
+        $rule = [];
+        $validator = Validator::make(Input::all(), $rule);
+        if ($validator->fails()){
+            return back()->withErrors($validator->messages())->withInput();
+        }
+        $combo = Combo::find($id);
+        if (!$combo) {
+            return view('error.404');
+        } else {
+            $combo->title=Input::get('title');
+            $slug = Input::has('slug') ? Input::get('slug', $combo->title) : str_slug(Input::get('title'));
+            $combo->slug = $combo->checkSlug($slug, $id);
+            $combo->link = Input::get('link');
+            $combo->status = intval(Input::get('status'));
+
+            $combo->save();
+            $combo->cleanCache();
+            return redirect('/combos')->withSuccess('Tạo mới thành công');
+        }
+    }
+
+    public function destroy($id) {
+        $combo = Combo::find($id);
+        if (!empty($combo->image)) {
+            $full_item_photo_dir = config('image.image_root').'/pieces';
+            $size = config('image.sizes.equipments');
+            ImageLib::delete_image($full_item_photo_dir, $combo->image, $size);
+        }
+        $combo->cleanCache();
+        $combo->delete();
+        return redirect('/combos')->withSuccess('Xóa thành công');
+    }
+}
+
---
+++ app/Models/Combo.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Moloquent;
+use Cache;
+
+class Combo extends Moloquent {
+    use LanguageField;
+    protected $collection = 'combo';
+
+    public function urlPath($size = '') {
+        if ($this->image == '') {
+            return config('image.image_url_admin').'/back/images/thumb_default.png';
+        } else {
+            if ($size) {
+                return env("IMAGE_URL").'equipments/'.$this->image.'_'.$size.'.png';
+            } else {
+                return env("IMAGE_URL").'equipments/'.$this->image.'.png';
+            }
+        }
+    }
+
+    public function displayStatus() {
+        return $this->status == 1 ? 'Hiển thị' : 'Ẩn';
+    }
+
+    public function cleanCache(){
+        $cache_key = config('cache.key.combo').$this->slug;
+        Cache::forget($cache_key);
+    }
+
+    public function checkSlug($slug, $id = null) {
+        if ($id) {
+            $check = Category::where('_id', '!=', $id)->where('slug', $slug)->count();
+        } else {
+            $check = Category::where('slug', $slug)->count();
+        }
+        if ($check > 0) {
+            $slug = $slug.'-'.str_random(10);
+        }
+        return $slug;
+    }
+
+    public function getArrayInfo() {
+        $rels = array(
+            "id"   => $this->_id,
+            "title" => $this->title,
+            "slug" => $this->slug,
+            "link"  => $this->link,
+        );
+        return $rels;
+    }
+
+    public function getImages() {
+        $data = [];
+        if ($this->image) {
+            $data['Small'] = env("IMAGE_URL").'/equipments/'.$this->image.'_100x100.png';
+            $data['Medium'] = env("IMAGE_URL").'/equipments/'.$this->image.'_100x100.png';
+            $data['Large'] = env("IMAGE_URL").'/equipments/'.$this->image.'_600x600.png';
+        } else {
+            $data['Small'] = env("HOME_PAGE").'/images/thumb_default.png';
+            $data['Medium'] = env("HOME_PAGE").'/images/thumb_default.png';
+            $data['Large'] = env("HOME_PAGE").'/images/thumb_default.png';
+        }
+        return $data;
+    }
+
+    public function languageFields($lang = 'en') {
+        $data = [
+            [
+                'name' => 'Tiêu đề',
+                'key' => 'title_'.$lang,
+                'type' => 'text',
+                'required' => false,
+                'placehoder' => ''
+            ],
+            [
+                'name' => 'Đường dẫn hiển thị trên url',
+                'key' => 'slug_'.$lang,
+                'type' => 'text',
+                'required' => false,
+                'placehoder' => ''
+            ],
+            [
+                'name' => 'Nội dung',
+                'key' => 'content_'.$lang,
+                'type' => 'textarea',
+                'required' => false,
+                'placehoder' => ''
+            ]
+        ];
+        return $data;
+    }
+
+
+}
+
