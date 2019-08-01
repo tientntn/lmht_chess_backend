63cdac4738dffee2c19746976f73a40b1673b6d5
Tiến Nguyễn <nguyentientntn@gmail.com>
2019-08-01 11:13:57 +0700

    update webview

--- resources/views/template_webview.blade.php
+++ resources/views/template_webview.blade.php
@@ -5,7 +5,7 @@
     <title>LOL auto chess</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link href="/css/extra_webview.css?v=1.7" rel="stylesheet">
+    <link href="/css/extra_webview.css?v=1.8" rel="stylesheet">
 
     @yield('head')
   </head>
